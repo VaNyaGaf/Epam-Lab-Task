337ad8fe4f41b44c4af30f52c66db214541d7406
Vladyslav Romanchuk <senioroman4uk@gmail.com>
2019-06-14 18:52:54 +0300

    Add DbInitializer

---
+++ GameStore/GameStore.PL/DbInitializer.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using GameStore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace GameStore.PL
+{
+    public class DbInitializer : IHostedService
+    {
+        private readonly IServiceScopeFactory _serviceScopeFactory;
+
+        public DbInitializer(IServiceScopeFactory serviceScopeFactory)
+        {
+            _serviceScopeFactory = serviceScopeFactory;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            using (var scope = _serviceScopeFactory.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
+                await context.Database.EnsureCreatedAsync(cancellationToken);
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
